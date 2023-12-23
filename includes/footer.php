71516755222f90be067c1516f089a7c668985493
Drago1426 <peter.sheehen.c10396@mcast.edu.mt>
2023-12-23 09:23:25 +0100

    Created Website

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<footer class="footer">
+            <div class="container p-4">
+                <div class="row">
+                    <!-- Logo -->
+                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                        <img src="/RetroArc-Website-PHP/images/logo-placeholder.png" alt="Logo" style="width: 150px;">
+                    </div>
+
+                    <!-- Footer Text -->
+                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                        <p>&copy; 2023 RetroArc. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
