b685e051a3434e54c359db34da5b7173af1dbd4e
Drago1426 <peter.sheehen.c10396@mcast.edu.mt>
2023-12-27 21:47:00 +0100

    Added the add product feature

--- addProductHandler.php
+++ addProductHandler.php
@@ -13,12 +13,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Validation logic here...
 
     // SQL to insert data into products table
-    $sql = "INSERT INTO products (productName, description, typeId, consoleId, price, quantity, productImage) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO product (productName, description, typeId, consoleId, price, quantity, productImage) VALUES (?, ?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssiiidi", $productName, $description, $typeId, $consoleId, $price, $quantity, $productImage);
+    $stmt->bind_param("ssiiids", $productName, $description, $typeId, $consoleId, $price, $quantity, $productImage);
 
     if ($stmt->execute()) {
-        echo "Product added successfully.";
+        echo "<script>
+                alert('Product added successfully.');
+                window.location.href='addProduct.php';
+              </script>";
     } else {
         echo "Error: " . $stmt->error;
     }
